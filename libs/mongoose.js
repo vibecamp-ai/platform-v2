f06111e9263962c2ed60fc7d5f91d6afa7a1319e
Fantastic George <jorislam@gmail.com>
2025-03-10 14:36:27 -0300

    Save before troubleshooting auth

---
+++ libs/mongoose.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import User from "@/models/User";
+import Board from "@/models/Board";
+
+const connectMongo = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+	} catch (e) {
+		console.error("❌ Mongoose Error: " + e.message);
+	}
+};
+
+export default connectMongo;
